92f016137b2f0a8c413633af5e24c926a1eb4479
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-08-04 13:36:51 +0530

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,68 @@
+import { useState } from "react";
+import people from "./data";
+import "./App.css";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
 const App = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number < 0) {
+      return people.length - 1;
+    }
+    if (number > people.length - 1) {
+      return 0;
+    }
+    return number;
+  };
+
+  const prevPerson = () => {
+    setIndex((oldIndex) => {
+      let newIndex = checkNumber(oldIndex - 1);
+      return newIndex;
+    });
+  };
+  const nextPerson = () => {
+    setIndex((oldIndex) => {
+      let newIndex = checkNumber(oldIndex + 1);
+      return newIndex;
+    });
+  };
+
+  const randomPerson = () => {
+    let randomIndex = Math.floor(Math.random() * people.length);
+    if (randomIndex === index) {
+      randomIndex = randomIndex + 1;
+    }
+    setIndex(checkNumber(randomIndex));
+  };
+
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img className="person-img" src={image} alt="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="btn" onClick={randomPerson}>
+          Surprise Me
+        </button>
+      </article>
+    </main>
+  );
+};
+export default App;
